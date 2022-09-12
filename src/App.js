48321dc8a802be0d4c0eb843cf779005603173b3
Levan Burchuladze <levan.burchuladze@rokolabs.com>
2022-09-13 01:46:47 +0400

    g

--- src/App.js
+++ src/App.js
@@ -4,6 +4,43 @@ import classes from "./App.module.css"
 function App() {
   return (
     <div className={classes.wrapper}>
+      <div className={classes.drawer_overlay}>
+        <div className={classes.drawer}>
+          <h2>Корзина</h2>
+
+          <div className={classes.cart_buy}>
+            <img className={classes.cart_buy_photo} width={70} height={70} src="itemf.jpg" alt="item1" />
+            <div className={classes.cart_buy_text}>
+              <p>Мужские кроссовки Nike Air Max 279</p>
+              <b>12 999 руб.</b>
+            </div>
+            <button>X</button>
+          </div>
+
+          <div className={classes.cart_buy}>
+            <img className={classes.cart_buy_photo} width={70} height={70} src="itemf.jpg" alt="item1" />
+            <div className={classes.cart_buy_text}>
+              <p>Мужские кроссовки Nike Air Max 279</p>
+              <b>12 999 руб.</b>
+            </div>
+            <button>X</button>
+          </div>
+
+          <ul className={classes.res}>
+            <li className={classes.res_info}>
+              <span>Итого:</span>
+              <div className={classes.res_line}></div>
+              <b>21 499 руб.</b>
+            </li>
+            <li className={classes.res_info}>
+              <span>Налог 5%:</span>
+              <div  className={classes.res_line}></div>
+              <b>1074 руб.</b>
+            </li>
+          </ul>
+        </div>
+      </div>
+
       <Header/>
       <div className={classes.content}>
         <h1>Все кроссовки</h1>
